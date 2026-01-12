b81d72c9e805d972455f65f58556f31395b7c53f
John Apale <74929433+johnApale@users.noreply.github.com>
2026-01-11 18:46:36 -0800

    Initial Commit

---
+++ TravelJournal-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelJournal-iOS
+//
+//  Created by John Apale on 1/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelJournal-iOS/TravelJournal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelJournal_iOSApp.swift
+//  TravelJournal-iOS
+//
+//  Created by John Apale on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelJournal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
