a3b0233c3e29319033f2ff1ee056ac36a3765cd3
John Apale <74929433+johnApale@users.noreply.github.com>
2026-01-28 08:54:38 -0800

    refactor: use shared ImagePicker in PassportPhotoView

--- TravelJournal-iOS/Views/Auth/Registration/PassportPhotoView.swift
+++ TravelJournal-iOS/Views/Auth/Registration/PassportPhotoView.swift
@@ -387,48 +387,6 @@ private struct CornerBrackets: View {
     }
 }
 
-// MARK: - Image Picker Wrapper
-struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-    let sourceType: UIImagePickerController.SourceType
-    @Environment(\.dismiss) var dismiss
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.sourceType = sourceType
-        picker.delegate = context.coordinator
-        picker.allowsEditing = true
-        return picker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-        let parent: ImagePicker
-        
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let editedImage = info[.editedImage] as? UIImage {
-                parent.image = editedImage
-            } else if let originalImage = info[.originalImage] as? UIImage {
-                parent.image = originalImage
-            }
-            parent.dismiss()
-        }
-        
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            parent.dismiss()
-        }
-    }
-}
-
 // MARK: - Preview
 #Preview {
     PassportPhotoView(
